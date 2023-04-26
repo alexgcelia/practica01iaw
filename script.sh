779040bf7ad27aabb7eb6032cf9899c11886c511
Alex Giménez <agimagu1601@g.educaand.es>
2023-04-26 12:32:42 +0000

    Actualizar script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "Hola Mundo"
+
+#Habilita que se muestre lps comandos ejecutados en el script
+set -x
+
+# Actualizamos los repositorios
+apt update
+
+#Actualizamos los programas a la última versión disponible
+apt upgrade -y
+
+#Instalación Apache
+apt install apache2 -y
+
+#Instalación MySQL
+apt install mysql-server -y
