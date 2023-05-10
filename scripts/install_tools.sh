86080e745aec5b71ddb941c946f6382f7311ab02
Alex Giménez <agimagu1601@g.educaand.es>
2023-05-10 12:30:43 +0000

    Creación base de datos y usuario phpMyAdmin

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,14 +3,21 @@
 #Habilita que se muestre lps comandos ejecutados en el script
 set -x
 
+#--------------------------------------
+#### VARIABLES DE CONFIGURACIÓN ####
+PHPMYADMIN_USER=alexg
+PHPMYADMIN_PASS=alexg
+#--------------------------------------
+
 # Actualizamos los repositorios
 apt update
 
 #Actualizamos los programas a la última versión disponible
 apt upgrade -y
 
+#--------------------------------------
 #Instalación phpMyAdmin
-#--------------------------
+#--------------------------------------
 
 #Paso 1. Instalación de las dependencias
 apt install php-mbstring php-zip php-gd php-json php-curl -y
@@ -22,4 +29,24 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-language
 apt install unzip -y
 
 #Paso 4. Descomprimimos el código fuente de phpMyAdmin
-unzip /tmp/phpmyadmin.zip -d /var/www/html
+unzip -o /tmp/phpmyadmin.zip -d /var/www/html
+
+#Paso 5. Eliminamos instalaciones previas de phpMyAdmin
+rm -rf /var/www/html/phpmyadmin
+
+#Paso 6. Renombramos el directorio de phpMyAdmin
+mv /var/www/html/phpMyAdmin-5.2.1-all-languages /var/www/html/phpmyadmin
+
+#Paso 7. Creamos el archivo de configuración a partir del archivo de ejemplo
+mv /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/phpmyadmin/config.inc.php
+
+#Paso 8. Creamos la base de datos de phpMyAdmin
+mysql -u root < /var/www/html/phpmyadmin/sql/create_tables.sql
+
+#Paso 9. Creamos un usuario en MySQL para phpMyAdmin
+mysql -u root <<< "DROP USER IF EXISTS $PHPMYADMIN_USER@localhost;"
+mysql -u root <<< "CREATE USER $PHPMYADMIN_USER@localhost IDENTIFIED BY '$PHPMYADMIN_PASS';"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON phpmyadmin.* TO $PHPMYADMIN_USER@localhost;"
+
+#Modificamos el propietario y el grupo
+chown -R www-data:www-data /var/www/html
